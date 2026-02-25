8f2543cb8718103e5f2d540ba7b4beba9522cab8
Vasudha Shivane <shivanevasudha@gmail.com>
2026-02-25 14:16:18 +0530

    added auth and bcrypt

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
 const express = require('express');
-const app = express();
-const { a } = require('framer-motion/client');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/order');
-const mongoose = require('mongoose');
 
-mongoose.connect(
-    'mongodb+srv://root:' + process.env.MONGO_ATLAS_PW + '@cluster-api.j51ouqk.mongodb.net/?appName=cluster-api',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
+const app = express();
 
-//middleware
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://root:' + process.env.MONGO_ATLAS_PW +
+      '@cluster-api.j51ouqk.mongodb.net/restapi?retryWrites=true&w=majority'
+    );
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectDB();
+app.use('/uploads', express.static('uploads'));
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-
+// CORS
 app.use((req, res , next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
     if(req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
-    }       
+    }
+
+    next();
 });
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+// 404
 app.use((req, res , next) => {
     const error = new Error('Not found');
     error.status = 404;
     next(error);
 });
 
+// Global error
 app.use((error, req, res , next) => {
-    res.status(error.status || 500);
-    res.json({
+    res.status(error.status || 500).json({
         error: {
             message: error.message
         }
